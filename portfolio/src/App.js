9179b4343a326003559483ee3059d77bed68b4de
David Katz <15Dkatz@shcp.edu>
2019-01-06 22:20:49 -0800

    Remove portfolio submodule and add subdirectories

---
+++ portfolio/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is David. I'm a software engineer.</p>
+        <p>I'm always looking forward to working on meaningful projects.</p>
+        {
+          this.state.displayBio ? (
+            <div>
+              <p>I live in San Francisco, and code every day.</p>
+              <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
+              <p>Besides coding, I also love music and ramen!</p>
+              <button onClick={this.toggleDisplayBio}>Show less</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={this.toggleDisplayBio}>Read more</button>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+export default App;
