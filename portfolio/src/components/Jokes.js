04a268373809df98dfb73d93e2e79401da69b9f8
David Katz <15Dkatz@shcp.edu>
2019-01-10 21:22:37 -0800

    Modify Jokes

--- portfolio/src/components/Jokes.js
+++ portfolio/src/components/Jokes.js
@@ -10,13 +10,15 @@ class Jokes extends Component {
   componentDidMount() {
     fetch('https://official-joke-api.appspot.com/random_joke')
       .then(response => response.json())
-      .then(json => this.setState({ joke: json }));
+      .then(json => this.setState({ joke: json }))
+      .catch(error => alert(error.message));
   }
 
   fetchJokes = () => {
     fetch('https://official-joke-api.appspot.com/random_ten')
       .then(response => response.json())
-      .then(json => this.setState({ jokes: json }));
+      .then(json => this.setState({ jokes: json }))
+      .catch(error => alert(error.message));
   }
 
   render() {
