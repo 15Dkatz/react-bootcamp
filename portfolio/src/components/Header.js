023a125b720b0be741ffe5898cb12424024bba6c
David Katz <15Dkatz@shcp.edu>
2019-01-11 23:56:31 -0800

    Include Music Master in Portfolio

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -13,6 +13,7 @@ const Header = ({ children }) => {
       <div>
         <h3 style={style}><Link to='/'>Home</Link></h3>
         <h3 style={style}><Link to='/jokes'>Jokes</Link></h3>
+        <h3 style={style}><Link to='/music-master'>Music Master</Link></h3>
       </div>
       {children}
     </div>
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,6 +4,7 @@ import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import Jokes from './components/Jokes';
+import MusicMaster from './projects/music-master';
 import Header from './components/Header';
 import './index.css';
 
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Switch>
       <Route exact path='/' render={() => <Header><App /></Header>} />
       <Route path='/jokes' render={() => <Header><Jokes /></Header>} />
+      <Route path='/music-master' render={() => <Header><MusicMaster /></Header>} />
     </Switch>
   </Router>,
   document.getElementById('root')
