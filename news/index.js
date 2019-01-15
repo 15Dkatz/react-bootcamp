5958a96692fb36a1d755cc1c1a11ec728d570625
David Katz <15Dkatz@shcp.edu>
2019-01-14 23:22:26 -0800

    Add request within request for news

--- news/index.js
+++ news/index.js
@@ -1,9 +1,50 @@
 const express = require('express');
+const request = require('request');
+const stories = require('./stories');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Request details. Method:', req.method, 'Original url:', req.originalUrl);
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  next();
+});
+
 app.get('/ping', (req, res) => {
-  res.send('pong');
+  res.send('pong!');
+});
+
+app.get('/stories', (req, res) => {
+  res.json(stories);
+});
+
+app.get('/stories/:title', (req, res) => {
+  const { title } = req.params;
+
+  res.json(stories.filter(story => story.title.includes(title)));
+});
+
+app.get('/topstories', (req, res, next) => {
+  request(
+    { url: 'https://hacker-news.firebaseio.com/v0/topstories.json' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return next(new Error('Error requesting top stories'));
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ type: 'error', message: err.message });
 });
 
 const PORT = 3000;
